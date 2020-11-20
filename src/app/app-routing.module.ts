40cf52a9858ceb9c11ec197db1ee2a19185de5fc
Nikita Tysiachnyi <nikita.tysiachnyi@extreme-idea.com>
2020-11-21 00:42:10 +0200

    added routing, added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ErrorPageComponent} from './pages/error-page/error-page.component';
+import {InfoPageComponent} from './pages/info-page/info-page.component';
 import {MainPageComponent} from './pages/main-page/main-page.component';
-
+import {SharePageComponent} from './pages/share-page/share-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/index', pathMatch: 'full'},
   {
     path: 'index',
-    component: MainPageComponent
-  }
+    component: MainPageComponent,
+  },
+  {
+    path: 'info',
+    component: InfoPageComponent,
+  },
+  {
+    path: 'share',
+    component: SharePageComponent,
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainPageComponent } from './pages/main-page/main-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainPageComponent} from './pages/main-page/main-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { InfoPageComponent } from './pages/info-page/info-page.component';
+import { SharePageComponent } from './pages/share-page/share-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent
-  ],
+  declarations: [AppComponent, MainPageComponent, ErrorPageComponent, InfoPageComponent, SharePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
