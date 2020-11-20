533961a158cb3535eb036dd4ad52f3d315b6a1c7
Nikita Tysiachnyi <nikita.tysiachnyi@extreme-idea.com>
2020-11-21 00:10:53 +0200

    Added material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainPageComponent} from './pages/main-page/main-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/index', pathMatch: 'full'},
+  {
+    path: 'index',
+    component: MainPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
